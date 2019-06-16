93cceb7107bb4d6bd1cf9ee81147dd129e8f3e1b
Coverkincel <coverkincel@gmail.com>
2019-06-16 05:26:21 +0300

    Updates

---
+++ precache-manifest.5d070b054accccd2e39fa2d2e6be71a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4511b7df1280a026814d694e166e47",
+    "url": "/Triviaquizor/index.html"
+  },
+  {
+    "revision": "ffb906a0a40feead2faa",
+    "url": "/Triviaquizor/static/css/main.a4a8cdad.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/Triviaquizor/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "ffb906a0a40feead2faa",
+    "url": "/Triviaquizor/static/js/main.db0bddb3.chunk.js"
+  },
+  {
+    "revision": "0371343d3cc18ad0fa89",
+    "url": "/Triviaquizor/static/js/runtime~main.86b3984b.js"
+  }
+]);
